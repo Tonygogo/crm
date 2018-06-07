3679dfdeaa190a219ec9e74200935daf999cca8a
Tonygogo <kl2422@163.com>
2018-06-07 08:55:56 +0800

    初始化营销管理app

--- crm/settings.py
+++ crm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sales.apps.SalesConfig',
     'system.apps.SystemConfig',
     'index.apps.IndexConfig',
     'django.contrib.admin',
--- crm/urls.py
+++ crm/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('sales/', include('sales.urls')),
     path('system/', include('system.urls')),
     path('', include('index.urls')),
 
